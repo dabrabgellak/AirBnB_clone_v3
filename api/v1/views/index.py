771091590e512f3662cc8f4276d2291a796c22cc
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-28 15:17:17 -0500

    add implementation for task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,20 @@ def status():
     Return status code
     '''
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    '''
+    Return count stats
+    '''
+    stats_count = {
+        "amenities": 47,
+        "cities": 36,
+        "places": 154,
+        "reviews": 718,
+        "states": 27,
+        "users": 31
+    }
+
+    return jsonify(**stats_count)
