b956f2606dc1921e640364663dabb4ea38f73f55
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-29 22:10:39 -0500

    refactor app.py for task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "*"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
