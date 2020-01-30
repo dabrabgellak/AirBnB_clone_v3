3c868b3dd64c30c4e49955a189f5e0531b36091c
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-29 20:20:00 -0500

    add task 13 and refactor error_not_found

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ module app
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -18,10 +20,7 @@ def teardown_db(self):
 
 @app.errorhandler(404)
 def error_not_found(error):
-    return make_response(jsonify(
-        {
-            "error": "Not found"
-        }), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
